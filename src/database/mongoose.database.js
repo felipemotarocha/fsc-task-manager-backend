8a676e515158d1e7ddc9b51d9713a3f1c261b7e9
Felipe Rocha <felipemota.rocha@gmail.com>
2022-08-30 11:37:31 -0300

    chore: change mongo url

--- src/database/mongoose.database.js
+++ src/database/mongoose.database.js
@@ -1,18 +1,15 @@
 const mongoose = require("mongoose");
 
 const connectToDatabase = async () => {
-    await mongoose.connect(
-        `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@fsctaskmanagercluster.6wsaf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
-        (error) => {
-            if (error) {
-                return console.log(
-                    `Could not connect to MongoDB: ${error.message}`
-                );
-            }
-
-            return console.log("Connected to MongoDB!");
+    await mongoose.connect(process.env.MONGO_URL, (error) => {
+        if (error) {
+            return console.log(
+                `Could not connect to MongoDB: ${error.message}`
+            );
         }
-    );
+
+        return console.log("Connected to MongoDB!");
+    });
 };
 
 module.exports = connectToDatabase;
