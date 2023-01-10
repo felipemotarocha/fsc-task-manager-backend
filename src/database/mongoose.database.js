bfe07624467f48b4947a7149c29102dd54643122
Felipe Rocha <felipemota.rocha@gmail.com>
2023-01-10 00:05:42 -0300

    fix: adjust mongo connection string

--- src/database/mongoose.database.js
+++ src/database/mongoose.database.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
 
 const connectToDatabase = async () => {
-    await mongoose.connect(process.env.MONGO_URL, (error) => {
-        if (error) {
-            return console.log(
-                `Could not connect to MongoDB: ${error.message}`
-            );
-        }
+    await mongoose.connect(
+        `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@fsctaskmanagercluster.nda0y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+        (error) => {
+            if (error) {
+                return console.log(
+                    `Could not connect to MongoDB: ${error.message}`
+                );
+            }
 
-        return console.log("Connected to MongoDB!");
-    });
+            return console.log("Connected to MongoDB!");
+        }
+    );
 };
 
 module.exports = connectToDatabase;
